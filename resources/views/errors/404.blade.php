1b31a75ed2f5f58bfbd470d3a367276d1a802383
Ronaldo Lima <2dlima@2d-Lima.local>
2016-09-18 23:53:53 -0300

    404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.site')
+
+@section('content')
+
+    <div class="plano biografias">
+        <div class="container">
+
+                <div class="frase">
+                    <div class="col-md-4">
+                        <span class="hr">
+                            <img src="./img/dotted.png" alt="" style="max-width:100%;">
+                        </span>
+                    </div>  
+
+                    <div class="col-md-4 text-md-center">
+                        <h2 class="planodecidade">404</h2>
+                    <strong>Página não encontrada</strong>
+
+                    </div> 
+
+                     <div class="col-md-4">
+                        <span class="hr">
+                            <img src="./img/dotted.png" alt="" style="max-width:100%;">
+                        </span>
+                    </div>  
+
+                </div>
+              
+        </div>
+    </div>
+
+    <section id="fabricio" class="fabricio">
+        <div class="container">
+            
+                <p class="">
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                </p>
+
+        </div>
+    </section>
+
+
+
+    
+
+@endsection
